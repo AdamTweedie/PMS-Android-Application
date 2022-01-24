a9bd92d739bb8b8577e458e1d8c78e9b75c6712c
mrAdamTweedie <adamgtweedie@gmail.com>
2022-01-24 18:47:06 +0000

    new files

---
+++ app/src/main/java/com/deitel/pms/ButtonFunctions.java
@@ -0,0 +1,26 @@
+package com.deitel.pms;
+
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+
+public class ButtonFunctions {
+
+    /***
+     * For buttons which are text only, change color onClick for 0.1 second.
+     * @param v View - the button passed as an object.
+     */
+    public static void textButtonColorChange(View v) {
+        v.setEnabled(true);
+        Button button = (Button) v;
+        button.setTextColor(Color.parseColor("#B185DB"));
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button.setTextColor(Color.parseColor("#6247AA"));
+            }
+        }, 250); // Millisecond 100 = 0.1 sec
+    }
+}
