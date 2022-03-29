f153dd5e23e2688232a93757c5421c187eecb40c
mrAdamTweedie <adamgtweedie@gmail.com>
2022-03-29 18:17:02 +0100

    Kanban testing

---
+++ app/src/androidTest/java/com/deitel/pms/student/kanban/KanbanBoardTest.java
@@ -0,0 +1,15 @@
+package com.deitel.pms.student.kanban;
+
+import junit.framework.TestCase;
+
+public class KanbanBoardTest extends TestCase {
+
+    public void testOnCreateView() {
+    }
+
+    public void testOnViewCreated() {
+    }
+
+    public void testGetDataFromSharedPrefs() {
+    }
+}
