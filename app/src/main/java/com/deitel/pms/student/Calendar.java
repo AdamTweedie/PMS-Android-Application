475a506955825545d57961a8bbcad0af86414407
mrAdamTweedie <adamgtweedie@gmail.com>
2022-04-06 01:05:54 +0100

    Calendar functionality

--- app/src/main/java/com/deitel/pms/student/Calendar.java
+++ app/src/main/java/com/deitel/pms/student/Calendar.java
@@ -2,10 +2,12 @@ package com.deitel.pms.student;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,9 +16,16 @@ import androidx.fragment.app.Fragment;
 
 import com.deitel.pms.R;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import sun.bob.mcalendarview.MCalendarView;
+import sun.bob.mcalendarview.listeners.OnDateClickListener;
+import sun.bob.mcalendarview.vo.DateData;
+
 public class Calendar extends Fragment {
 
-    private Button btnGoBack;
+    private ImageButton btnGoBack;
 
 
     @Nullable
@@ -33,7 +42,33 @@ public class Calendar extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         // functionality
-        btnGoBack = (Button) view.findViewById(R.id.btnGoBack);
+        btnGoBack = (ImageButton) view.findViewById(R.id.btnGoBack);
+        EditText day = (EditText) view.findViewById(R.id.calendarDay);
+        EditText month = (EditText) view.findViewById(R.id.calendarMonth);
+        EditText year = (EditText) view.findViewById(R.id.calendarYear);
+
+        MCalendarView calendarView = (MCalendarView) view.findViewById(R.id.calendarView);
+
+        ArrayList<DateData> dates = new ArrayList<>();
+
+        dates.add(new DateData(2022,05,9));
+        dates.add(new DateData(2022,05,7));
+
+        for (DateData date : dates) {
+            calendarView.markDate(date.getYear(), date.getMonth(), date.getDay());
+        }
+
+        calendarView.setOnDateClickListener(new OnDateClickListener() {
+            @Override
+            public void onDateClick(View view, DateData date) {
+
+                day.setText(String.valueOf(date.getDay()));
+                month.setText(String.valueOf(date.getMonth()));
+                year.setText(String.valueOf(date.getYear()));
+            }
+        });
+
+        Log.d("marked dates:-","" + calendarView.getMarkedDates()); //get all marked dates.
 
 
         btnGoBack.setOnClickListener(new View.OnClickListener() {
