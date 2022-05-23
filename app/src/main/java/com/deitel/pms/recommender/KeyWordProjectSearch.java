095e183ea435e40fa151d017b6697e177754d945
mrAdamTweedie <adamgtweedie@gmail.com>
2022-05-23 15:53:03 +0100

    re-formatting code

--- app/src/main/java/com/deitel/pms/recommender/KeyWordProjectSearch.java
+++ app/src/main/java/com/deitel/pms/recommender/KeyWordProjectSearch.java
@@ -8,11 +8,8 @@ public class KeyWordProjectSearch {
     public static ArrayList<Integer> Search(ArrayList<ArrayList<String>> projectData, String keyWord) {
 
         ArrayList<Integer> indexList = new ArrayList<>();
-        // split the project list twice to speed up computation
-        // rounded to integer automatically by Java "/"
         List<ArrayList<String>> list1 = projectData.subList(0, projectData.size()/2);
-        List<ArrayList<String>> list2 = projectData.subList(projectData.size()/2,
-                projectData.size());
+        List<ArrayList<String>> list2 = projectData.subList(projectData.size()/2, projectData.size());
         Thread t1 = new Thread(() -> {
             for (ArrayList<String> project : list1) {
                 String projectAsString = project.toString().toLowerCase();
@@ -25,17 +22,14 @@ public class KeyWordProjectSearch {
         Thread t2 = new Thread(() -> {
             for (ArrayList<String> project : list2) {
                 String projectAsString = project.toString().toLowerCase();
-                // get original index of project list
                 int index = list1.size() + list2.indexOf(project);
                 if (projectAsString.contains(keyWord.toLowerCase())) {
                     indexList.add(index);
                 }
             }
         });
-        t1.start();
-        t2.start();
+        t1.start(); t2.start();
         try {
-            // wait for both threads to finish
             t2.join();
             return indexList;
         } catch (InterruptedException e) {
